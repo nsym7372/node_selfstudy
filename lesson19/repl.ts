cace5a99bf8796e45ff65599fd320f682916f007
nsym7372 <nsym7372@outlook.jp>
2022-02-19 23:56:07 +0900

    複数テーブルからデータ取得して出力できた

--- lesson19/repl.ts
+++ lesson19/repl.ts
@@ -1,8 +1,12 @@
 import { db } from "./db/models";
+import { Section } from "./db/models/section";
 
 db.Area.findAll({ include: [{ model: db.Section }] }).then((areas) => {
   areas.forEach((area) => {
-    // console.log(`${area.id}: ${area.name}`);
-    console.log(JSON.stringify(area));
+    const sections_name = area.sections.map((s) => {
+      const section = s as Section;
+      return section.name;
+    });
+    console.log(`${area.id}: ${area.name}: ${sections_name}`);
   });
 });
