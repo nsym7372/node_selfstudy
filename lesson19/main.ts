d15aa572ddda35d5891a9219bccc7e98c036b075
nsym7372 <nsym7372@outlook.jp>
2022-02-20 00:20:39 +0900

    ユーザー登録画面を作成

--- lesson19/main.ts
+++ lesson19/main.ts
@@ -7,12 +7,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(layout);
 app.set("view engine", "ejs");
 
-app.get("/user", userController.getFirstUser);
+app.get("/user", userController.create);
 app.get("/sample", (req: express.Request, res: express.Response) => {
-    res.render("sample");
+  res.render("sample");
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`server runnning on port: ${port}`);
+  console.log(`server runnning on port: ${port}`);
 });
