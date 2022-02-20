4624c79851e68c83e2547ab42e8bdf447e94d7db
nsym7372 <nsym7372@outlook.jp>
2022-02-20 11:57:49 +0900

    フォーム使ってデータ登録できた

--- lesson19/controllers/userController.ts
+++ lesson19/controllers/userController.ts
@@ -1,10 +1,46 @@
-import express, { NextFunction } from "express";
-import { nextTick } from "process";
+import express from "express";
 import { db } from "../db/models";
 
-export const create = (req: express.Request, res: express.Response) => {
-  //
-  res.render("user/create");
+type actionType = (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => void;
+
+export const create: actionType = (req, res, next) => {
+    //
+    res.render("user/create");
+};
+
+export const generate: actionType = (req, res, next) => {
+    //
+    db.User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        zipcode: req.body.zipcode,
+        password: req.body.password,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    })
+        .then((user) => {
+            res.locals.redirect = "/user";
+            res.locals.user = user;
+            next();
+        })
+        .catch((error) => {
+            console.log(`Error saving user: ${error.message}`);
+            next(error);
+        });
+};
+
+export const redirectView: actionType = (req, res, next) => {
+    const redirectPath = res.locals.redirect;
+    if (redirectPath) {
+        res.redirect(redirectPath);
+    } else {
+        next();
+    }
 };
 
 // export const create = (
--- lesson19/main.ts
+++ lesson19/main.ts
@@ -8,11 +8,12 @@ app.use(layout);
 app.set("view engine", "ejs");
 
 app.get("/user", userController.create);
+app.post("/user", userController.generate, userController.redirectView);
 app.get("/sample", (req: express.Request, res: express.Response) => {
-  res.render("sample");
+    res.render("sample");
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`server runnning on port: ${port}`);
+    console.log(`server runnning on port: ${port}`);
 });
