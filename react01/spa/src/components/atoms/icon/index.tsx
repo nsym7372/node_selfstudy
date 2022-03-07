65a97b9edaafd78262a29549c9441e4f7c32c4aa
nsym7372 <nsym7372@outlook.jp>
2022-03-07 14:10:23 +0900

    TrashCanIcon 初期値設定

--- react01/spa/src/components/atoms/icon/index.tsx
+++ react01/spa/src/components/atoms/icon/index.tsx
@@ -9,29 +9,25 @@ interface OptionProps {
     width: number;
     onClick: () => void;
     options: {};
-}
-
-// 必須
-export interface IconProps extends Partial<OptionProps> {
     image: "trash_can" | "chevron";
 }
 
-export interface ContainerProps extends IconProps {
+export interface ContainerProps extends Partial<OptionProps> {
     presenter: (props: PresenterProps) => JSX.Element;
 }
 
-export interface PresenterProps extends IconProps {
+export interface PresenterProps extends Partial<OptionProps> {
     src: string;
     className: string;
 }
 
-export const IconFactory: VFC<IconProps> = (iconProps) => {
+export const IconFactory: VFC<Partial<OptionProps>> = (optionProps) => {
     return (
         <IconContainer
             presenter={(presetnerProps: PresenterProps) => (
                 <IconPresenter {...presetnerProps} />
             )}
-            {...iconProps}
+            {...optionProps}
         />
     );
 };
@@ -43,6 +39,35 @@ IconFactory.defaultProps = {
     options: {},
 };
 
-export const TrashCanIcon = () => {
-    return <IconFactory image={"trash_can"} onClick={() => {}} />;
+export const TrashCanIcon: VFC<Partial<OptionProps>> = ({
+    height = 32,
+    width = 32,
+    onClick = () => {},
+    options = {},
+    image = "trash_can",
+}) => {
+    return <IconFactory {...{ image, height, width, onClick, options }} />;
 };
+
+// [FYI] Propsにデフォルト値設定
+// export const Sample = ({
+//     hoge = "sample",
+//     fuga,
+// }: {
+//     hoge?: string;
+//     fuga: number;
+// }) => {
+//     //
+//     console.log(hoge);
+//     console.log(fuga);
+//     return (
+//         <>
+//             <span>{hoge}</span>
+//             <span>{fuga}</span>
+//         </>
+//     );
+// };
+
+// export const tmp = () => {
+//     return <Sample fuga={1} />;
+// };
