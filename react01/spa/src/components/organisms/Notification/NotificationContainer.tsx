d0fe0d01f60067f7fb9aeee9db3323e58222250e
nsym7372 <nsym7372@outlook.jp>
2022-03-09 22:02:38 +0900

    削除イベントに削除対象を渡すよう変更

--- react01/spa/src/components/organisms/Notification/NotificationContainer.tsx
+++ react01/spa/src/components/organisms/Notification/NotificationContainer.tsx
@@ -2,5 +2,14 @@ import React, { VFC } from "react";
 import { ContainerProps } from ".";
 
 export const NotificationContainer: VFC<ContainerProps> = (props) => {
-    return props.presenter(props);
+    let { className, program, onClickDelete: deleteFunc } = props;
+    const onClickDelete = () => {
+        console.log(program);
+        deleteFunc();
+    };
+    return props.presenter({
+        className,
+        program,
+        onClickDelete,
+    });
 };
